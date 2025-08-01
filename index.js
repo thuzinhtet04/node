0b61786cd6f173d3d1e679e351e77312cc69f1f0
thuzinhtet <thuzinhtet.dev@gmail.com>
2025-08-01 23:23:42 +0630

    first run server

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const logEvents = require('./logEvents')
-const EventEmitter = require('events')
+const EventEmitter = require('events');
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const fsPromises = require('fs').promises;
 class  MyEmitter extends  EventEmitter{
-
 }
-//initialize obj
-const myEmitter = new MyEmitter();
 
-myEmitter.on('log' , (msg) => logEvents(msg)
-)
-setTimeout(()=> {
-    //emit event
-    myEmitter.emit('log' , 'heo this is emit message')
-} ,1000 )
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req  , res) => {
+   console.log(req.url , req.method)
+})
+server.listen(PORT , () => console.log(`server running on port ${PORT}`))
+//initialize obj
+// const Emitter = new MyEmitter();
+//
+// Emitter.on('log' , (msg) => logEvents(msg)
+// )
