63998323c4a4627c3ce9ec1f1efa1a6d12da08b9
thuzinhtet <thuzinhtet.dev@gmail.com>
2025-08-01 23:36:06 +0630

    first route with switch statement and response html

--- index.js
+++ index.js
@@ -10,9 +10,27 @@ class  MyEmitter extends  EventEmitter{
 const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req  , res) => {
+
    console.log(req.url , req.method)
+   let filePath;
+   switch (req.url){
+      case '/' :
+         res.statusCode = 200;
+      filePath = path.join(__dirname , 'view' , 'index.html');
+      console.log(filePath , 'filepaht')
+      fs.readFile(filePath , 'utf8' , (err , data)=> {
+         console.log(data , 'data')
+         // if (err) throw err;
+         res.end(data);
+      });
+      break;
+   }
+
 })
 server.listen(PORT , () => console.log(`server running on port ${PORT}`))
+
+
+
 //initialize obj
 // const Emitter = new MyEmitter();
 //
